f3a6ddee2020833b4f22ad1753623090c0986e03
Ra01 <raul.aliaga.s@tecsup.edu.pe>
2021-11-24 22:20:46 -0500

    Comentarios nuevo

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,7 @@ name = "Rafael"
 console.log(name)
 
 // Hola Rafita como estas 
+
+// git add . -> Para agregar archivos
+// git commit -m "Envias mensaje" -> PAara conformar los cambios o enviar los cambios con un mensaje
+// git add remote origin "url:htttps:bckhsvs" -> Para enviar todo los archivos al servidor repositorio nube
